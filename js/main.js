f2a101d561df92118f38f8d00176c0cff98a7173
af737100-creator <af737100@gmail.com>
2026-02-28 20:34:49 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,7 @@
-// ============================================
-// دوال مساعدة عامة
-// ============================================
 function showNotification(message, type = 'info') {
-    alert(message); // يمكن استبدالها بنظام إشعارات متطور
+    alert(message);
 }
 
-// استخراج معرف فيديو يوتيوب (تستخدم دالة YouTubeAPI)
 window.extractYouTubeId = (url) => YouTubeAPI.extractVideoId(url);
 window.formatTime = (seconds) => YouTubeAPI.formatTime(seconds);
-
 window.showNotification = showNotification;
